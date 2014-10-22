07530d3798178888ae5bc838814d6aa616c2e96a
nickcharles <nickcmorgan@gmail.com>
2014-10-21 21:27:19 -0400

    Clean up

--- start.py
+++ start.py
@@ -3,9 +3,10 @@ import yikbot
 import time
 
 # Latitude and Longitude of location where bot should be localized
-yLocation = pyak.Location("42.270340", "-83.742224")
+ybLocation = pyak.Location("42.270340", "-83.742224")
 
-yb = yikbot.YikBot("yikBot", yLocation)
+yb = yikbot.YikBot()
+yb.update_location(ybLocation)
 print "DEBUG: Registered yikBot with handle %s and id %s" % (yb.handle, yb.id)
 
 print "DEBUG: Going to sleep, new yakkers must wait ~90 seconds before they can act"
