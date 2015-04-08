3c3b7dd5041739cc578a131b62abab17b0ac4e8a
Al Johri <al.johri@gmail.com>
2015-04-08 18:27:19 -0500

    make locations into hash

--- simple.py
+++ simple.py
@@ -2,11 +2,10 @@ import pyak
 
 yakker = pyak.Yakker()
 print "New yakker registered with ID: %s" % yakker.id
-# 41.849994, -87.650006
-# lat	41.849994
-# long	-87.650006
-location = pyak.Location(41.849994, -87.650006)
-yakker.update_location(location)
+locations = {
+	"tech": pyak.Location(42.057796,-87.676634)
+}
+yakker.update_location(locations['tech'])
 yaks = yakker.get_yaks()
 for yak in yaks:
     print yak.time, yak.message_id, yak.message
