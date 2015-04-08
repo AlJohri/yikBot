85775918551dd35dda577eedbb5f352a2ee59434
Al Johri <al.johri@gmail.com>
2015-04-08 16:58:33 -0500

    set location to sbux evanston

--- simple.py
+++ simple.py
@@ -2,8 +2,9 @@ import pyak
 
 yakker = pyak.Yakker()
 print "New yakker registered with ID: %s" % yakker.id
-location = pyak.Location(42.059011, -87.676093)
+# 42.059011, -87.676093
+location = pyak.Location(42.049368, -87.682038)
 yakker.update_location(location)
 yaks = yakker.get_yaks()
 for yak in yaks:
-    print yak.message
+    print yak.time, yak.message_id, yak.message
